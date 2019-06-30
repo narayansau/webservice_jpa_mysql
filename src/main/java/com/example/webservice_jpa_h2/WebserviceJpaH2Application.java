a29056b7efd941b1770b6c83a7d12d1b81cfc85c
Narayan Sau <narayansau@yahoo.com>
2019-06-30 14:50:44 +0530

    initial commit

---
+++ src/main/java/com/example/webservice_jpa_h2/WebserviceJpaH2Application.java
@@ -0,0 +1,14 @@
+package com.example.webservice_jpa_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceJpaH2Application{
+
+    public static void main(String[] args) {
+
+        SpringApplication.run( WebserviceJpaH2Application.class, args );
+    }
+
+}
---
+++ src/test/java/com/example/webservice_jpa_h2/WebserviceJpaH2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webservice_jpa_h2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebserviceJpaH2ApplicationTests{
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
